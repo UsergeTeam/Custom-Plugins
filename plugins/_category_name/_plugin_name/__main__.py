0664944cff42f7c430726dbf3e6691522a6c2955
rking32 <sljohnwick@gmail.com>
2022-03-06 10:56:24 +0530

    typo

--- plugins/_category_name/_plugin_name/__main__.py
+++ plugins/_category_name/_plugin_name/__main__.py
@@ -6,8 +6,8 @@ from userge import userge, Message
 from .. import _plugin_name as test
 
 
-@userge.on_cmd("test", about="my first plugins")
-async def first_commands(message: Message) -> None:
+@userge.on_cmd("test", about="my first command")
+async def first_command(message: Message) -> None:
     """ this thing will be used as command doc string """
 
     test.Dynamic.TIMEOUT = 90
